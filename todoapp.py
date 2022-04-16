c362e1d67524e9577dba357e423db1aaef6ddd4f
OrlandoG88 <98724886+OrlandoG88@users.noreply.github.com>
2022-04-16 16:17:49 -0400

    first commit

---
+++ todoapp.py
@@ -0,0 +1,65 @@
+from Flask import render_template, Flask, request
+import re
+
+
+app = Flask(__name__)
+
+class Task():
+    def __init__(self, task, email, priority):
+        self.task = task
+        self.email = email
+        self.priority = priority
+
+@app.route('/')
+def index():
+
+    return render_template('index.html', to_do= to_do)
+
+@app.route('/submit', methods = ['POST'])
+def submit():
+    global message
+    global to_do
+    task = request.form['task']
+    email = request.form['email']
+    priority = request.form['priority']
+
+    if task == '':
+        message = 'Unable to Add Task - Task Field is Blank'
+        return redirect("/")
+
+    email_reg = r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)'
+
+    if email == '':
+        message = 'Unable to Add Task - Email Field is Blank'
+        return redirect("/")
+
+    elif not re.search(email_reg, email):
+        message = 'Unable to Add Task - Email Format is Not Valid'
+        return redirect("/")
+
+    if priority == 'Blank':
+        message = 'Unable to Add Task - Priority Was Not Set'
+        return redirect("/")
+
+    new_task = Task(task, email, priority)
+
+    to_do.append(new_task)
+    message = ''
+
+    return redirect("/")
+
+
+@app.route('/clear', methods = ['POST'])
+def clear():
+    global todo_list
+    global message
+
+    todo_list = []
+    message = ''
+
+    return redirect("/")
+
+
+
+if __name__ == '__main__':
+    app.run()
